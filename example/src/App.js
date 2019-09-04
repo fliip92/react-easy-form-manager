3ea89a3a609ab980c06374143f6df745e00f0f3e
fliip92 <jufelipe.la@gmail.com>
2019-09-04 16:22:52 -0700

    init create-react-library@2.6.7

---
+++ example/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import ExampleComponent from 'easy-form-manager'
+
+export default class App extends Component {
+  render () {
+    return (
+      <div>
+        <ExampleComponent text='Modern React component module' />
+      </div>
+    )
+  }
+}
